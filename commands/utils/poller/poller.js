e29c704dd22310dfb0ac6bdb4c807fc73f9f542d
japneet <japneets@lambdatest.com>
2023-01-03 00:40:16 +0530

    handled downloads key

--- commands/utils/poller/poller.js
+++ commands/utils/poller/poller.js
@@ -40,15 +40,18 @@ function poll_build(lt_config, session_id, env) {
               }
               console.table(status);
               console.log(stats);
-              //Download the artefacts
-              let args = {
-                user: lt_config.lambdatest_auth.username,
-                access_key: lt_config.lambdatest_auth.access_key,
-                session_id: session_id,
-                env: env,
-                reject_unauthorized: lt_config.run_settings.reject_unauthorized,
-              };
-              reports(args);
+              //Download the artefacts if downloads is passed
+              if (lt_config.run_settings.downloads != "") {
+                let args = {
+                  user: lt_config.lambdatest_auth.username,
+                  access_key: lt_config.lambdatest_auth.access_key,
+                  session_id: session_id,
+                  env: env,
+                  reject_unauthorized:
+                    lt_config.run_settings.reject_unauthorized,
+                };
+                reports(args);
+              }
               if (
                 Object.keys(stats).length == 1 &&
                 (Object.keys(stats).includes("completed") ||
