dabf396b45e0b0356dc2bf6faead04938232270e
japneet <japneets@lambdatest.com>
2023-01-02 19:59:33 +0530

    auto download on sync

--- commands/utils/poller/poller.js
+++ commands/utils/poller/poller.js
@@ -1,6 +1,7 @@
 const poller = require("./build_stats.js");
 const async = require("async");
 const build_stats = require("./build_stats.js");
+const reports = require("../../../commands/generate_reports.js");
 var build_result = true;
 
 function poll_build(lt_config, session_id, env) {
@@ -39,6 +40,16 @@ function poll_build(lt_config, session_id, env) {
               }
               console.table(status);
               console.log(stats);
+              //Download the artefacts
+              let args = {
+                user: lt_config.lambdatest_auth.username,
+                access_key: lt_config.lambdatest_auth.access_key,
+                session_id: session_id,
+                env: env,
+                reject_unauthorized: lt_config.run_settings.reject_unauthorized,
+              };
+              console.log(args);
+              reports(args);
               if (
                 Object.keys(stats).length == 1 &&
                 (Object.keys(stats).includes("completed") ||
